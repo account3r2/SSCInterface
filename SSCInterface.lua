d707216ff28f14620b599833e1ef656bb66d8793
account3r2 <ilove1000bananas@gmail.com>
2015-03-02 19:41:34 -0800

    Added function to draw (top) bar.

--- SSCInterface.lua
+++ SSCInterface.lua
@@ -1,5 +1,14 @@
 local SSCI = {}				-- Main table.
 SSCI.terminal = {}
+SSCI.draw = {}
+
+SSCI.title = "(server)^2 Modification Interface"		-- Title of this modification.
+SSCI.version = "1.0.0"									-- Version number of this modification.
+SSCI.beta = false										-- If this build is a beta build.
+SSCI.alpha = true										-- If this build is an alpha build.
+
+local blank = ""
+local space = " "
 
 -- Load ansicolors
 function reqansicolors()
@@ -28,4 +37,15 @@ end
 function SSCI.terminal.getSize()
 	SSCI.terminal.getWidth()
 	SSCI.terminal.getHeight()
+end
+
+function SSCI.draw.bar()
+	io.output(io.stdout)
+	io.write(colors("%{reset reverse} " .. SSCI.title))
+
+	for i = 1, math.floor(SSCI.terminal.width - (#SSCI.title + #SSCI.version + 4 + (SSCI.alpha and 6 or 0) + (SSCI.beta and 5 or 0))), 1 do
+		io.write(colors("%{reset reverse} "))
+	end
+
+	io.write(colors("%{reset reverse} v" .. SSCI.version .. (SSCI.alpha and " Alpha" or blank) .. (SSCI.beta and " Beta" or blank) .. " %{reset}"))
 end
